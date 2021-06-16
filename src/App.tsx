48f920cbfbe063d2ba175b9589c100aad67143cb
Michal <michal.p.zarzycki@wp.pl>
2021-06-16 21:28:03 +0200

    Set up API get request

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_API_KEY
+const cities: string[] = ['Gdansk', 'London', 'Bogota', 'Santiago'];
+const fetchWeather = (city: string) => {
+ return  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+  .then((res: any) => {
+    console.log(res)
+    return res
+  }).catch((err: any) => {
+    console.log(err)
+    return err
+  })}
+
 function App() {
+  const [weathers, setWeathers] = useState<any[]>([])
+
+  useEffect(() => {
+    let weatherPromises = cities.map((city: any) => fetchWeather(city))
+
+    Promise.all([...weatherPromises]).then((values) => {
+     setWeathers([...values])
+    });
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+
+   </div>
   );
 }
 
